b0adc083880119efb308432869f3c017e914ec17
Kunal Mathur <kunalsrtk1404@gmail.com>
2023-03-22 02:05:56 +0530

    edited the title

--- src/components/Temp.js
+++ src/components/Temp.js
@@ -7,7 +7,7 @@ const Temp = () => {
 
   const getWeatherInfo = async () => {
     try {
-      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=86327b955e72700f364d4201872b5119`;
+      let url = `git `;
 
       let res = await fetch(url);
       let data = await res.json();
